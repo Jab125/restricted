c3e5e280f082228da849a61e6f9b7564dcc68930
JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET <75049883+JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET@users.noreply.github.com>
2021-09-16 11:05:40 +1000

    Change ExampleMod

--- src/main/java/com/jab125/forge/restricted/Restricted.java
+++ src/main/java/com/jab125/forge/restricted/Restricted.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.jab125.forge.restricted;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -18,13 +18,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("restricted")
+public class Restricted
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public Restricted() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
